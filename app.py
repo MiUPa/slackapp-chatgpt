4a1b008d78cdc53cef34c7b3c6647fa8cd1d805c
Miura <miura.mc23@gmail.com>
2024-05-08 15:02:03 +0900

    7.10

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+import re
+
+from langchain.chat_models import ChatOpenAI
+
 load_dotenv()
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -10,9 +14,16 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
 def handle_mention(event, say):
-    user = event["user"]
     thread_ts = event["ts"]
-    say(thread_ts=thread_ts, text=f"Hello <@{user}>!")
+    message = re.sub("<@.*", "", event["text"])
+
+    llm = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+    )
+
+    response = llm.predict(message)
+    say(thread_ts=thread_ts, text=response)
 
 
 if __name__ == "__main__":
